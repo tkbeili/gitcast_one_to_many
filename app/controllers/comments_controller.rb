1158b69e5bb85360d45e1ac51f310b99d19bf9fc
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-06-09 09:52:13 -0700

    Implement the comment create action
    We start implementing the create action in the `CommentsController` by defining the method and then finding the post record we're creating the comment for. Note that `params[:post_id]` is coming from the URL. The URL that submits to this action is `/posts/:post_id/comments`.
    
    We then instantiate an `@comment` variable with the params submitted taking into account using strong parameters.
    
    We then associate the `@comment` record with the `@post` using `@comment.post = @post`.
    
    If the `@comment` records saves correctly then we redirect back to the `@post` show page which is `post_path(@post)`.
    
    If the `@comment` doesn't save correctly then we render the `show.html.erb` in the `/views/posts` folder. For this we have to use the full path for it because if we do `render :show` it will render `/views/comments/show.html.erb`

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post          = Post.find params[:post_id]
+    comment_params = params.require(:comment).permit(:body)
+    @comment       = Comment.new comment_params
+    @comment.post  = @post
+    if @comment.save
+      redirect_to post_path(@post), notice: "Comment created"
+    else
+      render "/posts/show"
+    end
+  end
 end
